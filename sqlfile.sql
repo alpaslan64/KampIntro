584b23e035e0900e1e4f4d173a370da4a7a3d057
alpaslan64 <75975892+alpaslan64@users.noreply.github.com>
2021-01-28 18:12:51 +0300

    Add files via upload

---
+++ sqlfile.sql
@@ -0,0 +1,23 @@
+﻿--Select
+select ContactName,CompanyName,City from Customers
+
+Select * from Customers where City = 'London'
+
+select * from Products where categoryId=1 or categoryId=3
+
+select * from Products where categoryId=1 and UnitPrice>10
+
+select * from Products where CategoryID=1 order by UnitPrice asc --ascending (artan)   --descending (azalan)
+
+select count(*) Adet from Products where CategoryID=2
+
+select categoryId, count(*) from products where UnitPrice>20 group by CategoryID having count(*)<10
+
+select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName
+from Products inner join Categories on Products.CategoryID=Categories.CategoryID where Products.UnitPrice>10
+
+--DTO Data Transformation Object
+
+select * from Products  left join [Order Details] on Products.ProductID = [Order Details].ProductID inner join Orders o on o.OrderID= [Order Details].OrderID 
+
+select * from Customers c left join Orders o on c.CustomerID= o.CustomerID where o.CustomerID is null
